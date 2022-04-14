115125ead424bd37f1de87f47e5a051a351548bb
Rub <melikyan740@gmail.com>
2022-04-13 20:02:25 -0700

    change picture radius and text

--- index.js
+++ index.js
@@ -1,2 +1,51 @@
 
 // Select some elements...
+let header = document.querySelector('#page-header')
+
+header.style.textAlign = 'left'
+header.style.backgroundColor = 'red'
+header.style.borderRadius = '50px'
+
+
+
+
+let dogImages = document.querySelectorAll('.dog-image')
+for (let i = 0; i < dogImages.length; i++) {
+
+    dogImages[i].style.borderRadius = '50px'
+
+
+}
+
+
+let nameDog = document.querySelectorAll('.dog-name')
+for (let i = 0; i < nameDog.length; i++) {
+
+    nameDog[i].style.textAlign = 'left'
+
+
+}
+
+let footerSecond = document.querySelector('.footer')
+
+footerSecond.style.color = 'red'
+
+header.style.borderRadius = '50px'
+
+
+let pictures = document.querySelectorAll('.dog-image')
+
+for (let i = 0; i < dogImages.length; i++) {
+
+    dogImages[i].style.transform = 'scalex(-1)'
+
+}
+
+
+let head = document.body
+
+head.style.backgroundColor = 'lightgreen'
+
+
+
+
